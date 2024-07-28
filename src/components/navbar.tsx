b63639936a35b551de9ef397405525bcecde3ddf
MarianIsrael <marianwadia55@gmail.com>
2024-07-28 20:58:29 +0200

    added navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,26 @@
+import { navLists } from '@/constants'
+import { appleImg, bagImg, searchImg } from '@/utils'
+import Image from 'next/image'
 import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <header className='w-full flex py-5 items-center justify-between sm:px-5 px-10'>
+        <nav className='w-full flex screen-max-width flex-row items-center justify-between'>
+            <Image src={appleImg} alt='apple-logo' width={20} height={18} />
+
+            <div className='hidden md:flex flex-row gap-7 '>
+                {navLists.map(navItem=>(
+                    <p className='text-gray-500 hover:text-white transition-all text-sm font-medium cursor-pointer' key={navItem}>{navItem}</p>
+                ))}
+            </div>
+
+            <div className='flex flex-row items-center gap-4 md:gap-7'>
+                <Image src={searchImg} alt='search-icon' width={20} height={18} />
+                <Image src={bagImg} alt='bag-icon' width={20} height={18} />
+            </div>
+
+        </nav>
+    </header>
   )
 }
