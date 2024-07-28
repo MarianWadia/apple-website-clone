ee5b9eae178f5f867ed4fde3cd8dc373c6ecaf17
MarianIsrael <marianwadia55@gmail.com>
2024-07-29 01:36:44 +0200

    added video package for next && started hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import smallHero from "@/../videos/smallHero.mp4"
+import hero from "@/../videos/hero.mp4"
+import Video from 'next-video';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
 
 export default function Hero() {
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHero : hero)
+  useGSAP(()=>{
+    gsap.to('#title-hero-1',{
+      opacity: 1,
+      delay: 1.5
+    })
+  },[])
+
+  const handleSetVideoSrc = () => {
+    if(window.innerWidth < 760) {
+      setVideoSrc(smallHero)
+    }else{
+      setVideoSrc(hero)
+    }
+  }
+
+  useEffect(()=>{
+    window.addEventListener("resize", handleSetVideoSrc)
+  },[])
+
   return (
-    <div>Hero</div>
+    <section className='w-full nav-height bg-black relative'>
+      <div className='h-5/6 w-full flex-center flex-col'>
+        <p id='title-hero-1' className='hero-title'>iPhone 15 Pro</p>
+        {/* <div className='w-9/12 md:w-10/12'>
+          <Video src={videoSrc} autoPlay muted loop controls={false} key={videoSrc.createdAt} playsInline={true} className='pointer-events-none'/>
+        </div> */}
+      </div>
+    </section>
   )
 }
